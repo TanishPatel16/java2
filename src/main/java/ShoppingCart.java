1c254d5b2c8472376f626e2cfa895ea68b03e8f3
TanishPatel16 <tanish.patel@fiserv.com>
2023-10-26 09:39:28 -0400

    Ex5 B

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -2,9 +2,15 @@ import java.util.*;
 public class ShoppingCart {
     private ArrayList<Item> arr;
     private float totalCost;
+    private float shippingCost;
     public ShoppingCart(){
         this.arr = new ArrayList<Item>();
         this.totalCost = 0;
+    }
+
+    public ShoppingCart(float shippingCost){
+        this.arr = new ArrayList<Item>();
+        this.shippingCost = shippingCost;
     }
 
     public void addItem(Item item){
@@ -14,6 +20,10 @@ public class ShoppingCart {
     public void calculateTotal(){
         for(int i=0; i<this.arr.size(); ++i){
             totalCost += (this.arr.get(i).getCost() * this.arr.get(i).getQuantity());
+        }
+        totalCost += totalCost * 0.1;
+        if(totalCost < 10){
+            totalCost += this.shippingCost;
         }
     }
 
